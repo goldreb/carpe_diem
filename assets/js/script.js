90f9895b2d63d53bdb293f2571c0cfdb2a230491
Callie Nipper <callie.nipper@gmail.com>
2021-03-04 20:26:30 -0800

    Created quote API logic is js file. Added ids to HTML doc

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+let getQuote = function() {
+    let quoteAPI = `https://quote-garden.herokuapp.com/api/v3/quotes`;
 
+    fetch(quoteAPI)
+        .then(function (response) {
+            return response.json()}).then(function (data) {
+                console.log(data)
+                let index = Math.floor(Math.random() * data.data.length);
+                document.getElementById("quote").textContent = data.data[index].quoteText;
+                document.getElementById("author").textContent = data.data[index].quoteAuthor
+
+            });
+}
+getQuote()
