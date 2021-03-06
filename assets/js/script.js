51be986662516d5b18459bd332aa8139d2e88b68
JGrandbit <josseliner25@gmail.com>
2021-03-06 12:25:57 -0800

    went for a trivia instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+let getWeatherAPI= function (){
 
+let getWeatherAPI ='https://api.openweathermap.org/data/2.5/weather?q=portland&appid=9b679ef1fb211d4567f1a543dd9080d9';
+fetch(getWeatherAPI)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+}
+getWeatherAPI()
